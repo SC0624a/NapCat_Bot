a587a940d2b99c3a3ab0b007cf02ccc483f942eb
SC0624a <97958094+SC0624a@users.noreply.github.com>
2026-04-11 19:56:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ class Config:
         self.api_url = 'http://127.0.0.1:3000'  # API地址
         self.admin_id = 2163712324  # 管理员ID
         self.max_processed_cache = 1000  # 最大消息缓存数
-        self.doubao_api_key = '8c09bbf4-311a-4ca8-9678-6656774b1247'
+        self.doubao_api_key = '自行获取'
         self.doubao_model_id = 'doubao-seed-1-8-251228'
-        self.ws_token = 'sc20060624aaa'
-        self.api_token = 'sc20060624aaa'
+        self.ws_token = '你的wstoken'
+        self.api_token = '你的api token'
 
 config = Config()
